80212c8acea6bc52b0f0b4e770c32d9ad81bfa9d
Hans Kristanto <kristanto.hs@gmail.com>
2019-06-29 08:03:34 +0800

    Fix indentation

--- large_json_parser.py
+++ large_json_parser.py
@@ -2,38 +2,38 @@ import sys
 
 def parse_json(file):
 
-	def _parse_attribute(attr):
-		if attr.endswith(','):
-			value = attr[attr.index(':')+3:len(attr)-2]
-		else:
-			value = attr[attr.index(':')+3:len(attr)-1]
+    def _parse_attribute(attr):
+        if attr.endswith(','):
+            value = attr[attr.index(':')+3:len(attr)-2]
+        else:
+            value = attr[attr.index(':')+3:len(attr)-1]
 
-		return value
+        return value
 
-	try:
-		with open(file) as f:
-			row = ''
-			for line in f:
-				l = line.strip()
+    try:
+        with open(file) as f:
+            row = ''
+            for line in f:
+                l = line.strip()
 
-				if l in ['[', ']', '{']:
-					row = ''
-				elif l in ['}', '},']:
-					print(row)
-				else:
-					row += _parse_attribute(l) + ','
-	except IOError:
-		print('Error: File %s can not be found' % file)
+                if l in ['[', ']', '{']:
+                    row = ''
+                elif l in ['}', '},']:
+                    print(row)
+                else:
+                    row += _parse_attribute(l) + ','
+    except IOError:
+        print('Error: File %s can not be found' % file)
 
 
 def main():
-	try:
-		filename = sys.argv[1]
-	except IndexError:
-		print('Error: File location must be specified')
+    try:
+        filename = sys.argv[1]
+    except IndexError:
+        print('Error: File location must be specified')
 
-	parse_json(filename)
+    parse_json(filename)
 
 
 if __name__ == '__main__':
-	main()
+    main()
